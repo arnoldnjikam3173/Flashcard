70d781a2510c2a57ed4d687805bd44455528b2ee
fokouarnaud <fokouarnaud@gmail.com>
2022-06-21 12:13:50 +0100

    install package and add  data

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 
 import './App.css';
 import Flashcard from './components/Flashcard';
+import { useState,useEffect } from 'react';
 
-function App() {
+import wcc from 'world-countries-capitals';
+const App=() =>{
+  const [country,setCountry]= useState({});
+  const getData= ()=>{
+    const name=wcc.getRandomCountry();
+    const data= wcc.getCountryDetailsByName(name);
+    return data.length>0? data[0]:{};
+  }
+
+  useEffect(()=>{
+    const data=getData();
+    setCountry(data);
+  },[]);
+
+
   return (
-    <Flashcard/>
+    <Flashcard data={country}/>
   );
 }
 
